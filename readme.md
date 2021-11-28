a28593646d5b62f45d4b82520bb5fd0e9d0fa1f6
Erick Freitas <erickdefg@gmail.com>
2021-11-28 12:32:54 -0300

    Final Challege 🎊🎉

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Demo App
+
+<br/>
+<br/>
+
+<h1 align="center">
+  <img src="./global/demo/upfi.gif" />
+</h1>
--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -2,7 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fauna from 'faunadb';
 
 const { query } = fauna;
-const client = new fauna.Client({ secret: process.env.FAUNA_API_KEY });
+
+const client = new fauna.Client({
+  secret: process.env.FAUNA_API_KEY,
+
+  // US Region group, for example
+  domain: 'db.us.fauna.com',
+});
 
 interface ImagesQueryResponse {
   after?: {
